41524bad7432b7e04d839a5095b05d400c1b98bc
aRios22 <amrios@unicauca.edu.co>
2023-11-03 17:45:44 -0500

    First Commit

---
+++ src/main/java/com/unicauca/backendminiproyecto/BackendMiniProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.unicauca.backendminiproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMiniProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMiniProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicauca/backendminiproyecto/BackendMiniProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicauca.backendminiproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMiniProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
